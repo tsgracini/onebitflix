2f434565456f32badc154dfa28714666579f2a40
Thiago da Silva Gracini <thiago.gracini@outlook.com>
2023-01-07 12:28:29 -0300

    Atualizando o endpoint de cursos

--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -8,5 +8,25 @@ export const likeService = {
     })
 
     return like
+  },
+
+  delete: async (userId: number, courseId: number) => {
+    await Like.destroy({
+      where: {
+        userId,
+        courseId
+      }
+    })
+  },
+
+  isLiked: async (userId: number, courseId: number) => {
+    const like = await Like.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return like !== null ? true : false
   }
 }
