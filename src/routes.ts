270be8334a2a74e103b471e2ecefe97587db7aba
Thiago da Silva Gracini <thiago.gracini@outlook.com>
2023-01-10 06:31:57 -0300

    Obtendo informações do usuário logado

--- src/routes.ts
+++ src/routes.ts
@@ -34,5 +34,6 @@ router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes/:id', ensureAuth, likesController.delete)
 
 router.get('/users/current/watching', ensureAuth, usersController.watching)
+router.get('/users/current', ensureAuth, usersController.show)
 
 export { router }
